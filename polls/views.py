609e74bc3dc228fc2b344438395c7b9f4b42cb27
Chijindu Iwuchukwu <jendy36@gmail.com>
2018-12-25 18:17:02 +0100

    add html rendering and update the views file

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+# from django.template import loader, RequestContext
 # Create your views here.
 
 def index(request):
-	latest_questions = Question.objects.order_by('-pub_date')
-	output = ", ".join(q.question_text for q in latest_questions)
-	return HttpResponse(output)
+	latest_questions = Question.objects.order_by('-pub_date')[:5]
+	# template = loader.get_template('polls/index.html')
+	context = {'latest_questions': latest_questions}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("This is the detail view of the question: {}".format(question_id))

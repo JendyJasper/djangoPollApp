2681a38cbcc763929cc4d20702be3a827a10192d
Chijindu Iwuchukwu <jendy36@gmail.com>
2018-12-25 19:28:49 +0100

    update the get property

--- polls/views.py
+++ polls/views.py
@@ -11,7 +11,8 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("This is the detail view of the question: {}".format(question_id))
+	question = Question.objects.get(pk = question_id)
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	return HttpResponse("These are the results view of the question: {}".format(question_id))

1b64def925c745130ac580007c0794e8fa882cc9
Chijindu Iwuchukwu <jendy36@gmail.com>
2018-12-26 21:06:04 +0100

    use generic views

--- polls/urls.py
+++ polls/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
 	
 	# 127.0.0.1/polls
-	path('', views.index, name="index"),
+	path('', views.IndexView.as_view(), name="index"),
 
 	# 127.0.0.1/polls/1
-	path('<int:question_id>/', views.detail, name="detail"),
+	path('<int:pk>/', views.DetailView.as_view(), name="detail"),
 
 	# 127.0.0.1/polls/1/results
-	path('<int:question_id>/results', views.results, name="results"),
+	path('<int:pk>/results/', views.ResultsView.as_view(), name="results"),
 
 	# 127.0.0.1/polls/1/vote
-	path('<int:question_id>/vote', views.vote, name="vote"),
+	path('<int:question_id>/vote/', views.vote, name="vote"),
 
 ]
